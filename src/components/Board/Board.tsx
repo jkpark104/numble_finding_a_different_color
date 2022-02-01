f46dd4c6d88fee615986b87e356387c885777e87
jkpark104 <jkrang104@gmail.com>
2022-02-01 20:48:56 +0900

    fix: Fix BoardStyle
    1.Iimplement BoardComponent flex style

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,9 +1,9 @@
-import { BoardProps } from './Board.types';
+import { BoardProps, ItemProps } from './Board.types';
 
 export function Board({ children }: BoardProps): JSX.Element {
-  return <div className="board">{children}</div>;
+  return <div className="max-w-[22.5rem] flex flex-wrap justify-around">{children}</div>;
 }
 
-Board.Item = function (): JSX.Element {
-  return <div />;
+Board.Item = function ({ className }: ItemProps): JSX.Element {
+  return <div className={className} />;
 };
