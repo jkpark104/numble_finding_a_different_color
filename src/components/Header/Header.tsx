63b68bed83ee6e63b6df908952b305f6e4e8c0f8
jkpark104 <jkrang104@gmail.com>
2022-02-04 08:05:27 +0900

    feat: Implement Header Component
    1. Header Component
    2. Header.Title Component
    3. Header.GameInfos Component to represent informati`on of game

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,15 @@
-import { HeaderProps } from './Header.types';
+import { HeaderProps, HeaderGameInfosProps } from './Header.types';
 
 export function Header({ children }: HeaderProps): JSX.Element {
   return <header>{children}</header>;
 }
+
+Header.Title = function HeaderTitle(): JSX.Element {
+  return <h1 className="sr-only">Finding A Different Color</h1>;
+};
+
+Header.GameInfos = function GameInfos({ gameInfos }: HeaderGameInfosProps): JSX.Element {
+  const { stage, remainingTime, score } = gameInfos;
+
+  return <p>{`스테이지: ${stage}, 남은 시간: ${remainingTime}, 점수: ${score}`}</p>;
+};
--- src/components/Header/Header.types.ts
+++ src/components/Header/Header.types.ts
@@ -1,5 +1,10 @@
 import { ReactNode } from 'react';
+import { GameInfoProps } from '@/App/App.types';
 
 export interface HeaderProps {
   children: ReactNode;
+}
+
+export interface HeaderGameInfosProps {
+  gameInfos: GameInfoProps;
 }
