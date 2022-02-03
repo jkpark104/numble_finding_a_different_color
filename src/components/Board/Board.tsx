5980209fd5eda7d273797825a80b040df1f1f83b
jkpark104 <jkrang104@gmail.com>
2022-02-04 08:03:22 +0900

    feat: Implement Board Component
    1. Board Component
    2. Board.Items Component to represent colors

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,9 +1,30 @@
-import { BoardProps, ItemProps } from './Board.types';
+import { BoardProps, ItemsProps } from './Board.types';
 
 export function Board({ children }: BoardProps): JSX.Element {
   return <div className="max-w-[22.5rem] flex flex-wrap justify-around">{children}</div>;
 }
 
-Board.Item = function ({ className }: ItemProps): JSX.Element {
-  return <div className={className} />;
+Board.Title = function BoardTitle(): JSX.Element {
+  return <h2 className="sr-only">Board</h2>;
+};
+
+Board.Items = function BoardItem({ boardData, length, onClick }: ItemsProps): JSX.Element {
+  const { itemSize, indexOfanswer, rgbOfAnswer, rgbOfWrongAnswer } = boardData;
+
+  return (
+    <>
+      {Array.from({ length }, (_, index) => (
+        <div
+          key={index}
+          className="m-[0.125rem]"
+          style={{
+            width: itemSize,
+            height: itemSize,
+            backgroundColor: index === indexOfanswer ? rgbOfAnswer : rgbOfWrongAnswer,
+          }}
+          onClick={(): void => onClick(index === indexOfanswer)}
+        />
+      ))}
+    </>
+  );
 };
--- src/components/Board/Board.types.ts
+++ src/components/Board/Board.types.ts
@@ -1,9 +1,13 @@
 import { ReactNode } from 'react';
+import { BoardData } from '@/hooks/index';
 
 export interface BoardProps {
   children: ReactNode;
 }
 
-export interface ItemProps {
-  className: string;
+export interface ItemsProps {
+  boardData: BoardData;
+  length: number;
+
+  onClick: (isAnswer: boolean) => void;
 }
