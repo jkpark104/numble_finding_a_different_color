b09ca2c1ac730c2823066c393703431c8af835fc
jkpark104 <jkrang104@gmail.com>
2022-02-08 14:27:36 +0900

    fix: Modify codes
    1. delete dispatch
    2. change setInterval to setTimeout

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,15 +1,13 @@
 import { useEffect, Dispatch } from 'react';
-import { endGame, timeGoes } from '@/App/appReducer';
+import { timeGoes } from '@/App/appReducer';
 import { Action } from '@/App/appReducer.types';
 
 export const useTimer = (dispatch: Dispatch<Action>, remainingTime: number): void => {
   useEffect(() => {
-    const timerId = setInterval((): void => {
+    const timerId = setTimeout((): void => {
       dispatch(timeGoes());
     }, 1000);
 
-    if (remainingTime === 0) dispatch(endGame());
-
-    return (): void => clearInterval(timerId);
+    return (): void => clearTimeout(timerId);
   }, [dispatch, remainingTime]);
 };
