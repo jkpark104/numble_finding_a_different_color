e1dd1354f497c1aa12e0a48ed7ee4c7960dffee1
jkpark104 <jkrang104@gmail.com>
2022-02-09 20:44:34 +0900

    config: Modify craco.config
    1. set devtool option off in production env

--- craco.config.js
+++ craco.config.js
@@ -5,5 +5,12 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
     },
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    configure: (webpackConfig, { env, paths }) => {
+      return {
+        ...webpackConfig,
+        devtool: env === 'development' ? 'eval-cheap-source-map' : false,
+      };
+    },
   },
 };
