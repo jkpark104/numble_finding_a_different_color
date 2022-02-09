9596e7a9fd4c543ef444c9d12b84167181b0f505
jkpark104 <jkrang104@gmail.com>
2022-02-09 20:42:45 +0900

    fix: Change code location
    1. set codes about pollyfill to the top

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import './reportWebVitals';
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import '@/styles/global.css';
 import App from './App/App';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-import './reportWebVitals';
 
 render(
   <StrictMode>
